5b11be465d3068d68faf1eb7db24b9faf5bb656c
LARS243 <arsenka.243@mail.ru>
2025-02-21 17:23:31 +0300

    я не шиз

--- aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.cpp
+++ aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.cpp
@@ -4,29 +4,61 @@ using namespace std;
 using namespace sf;
 
 const int size_window = 1500;
-
+const int size_field_x = 80;
+const int size_field_y = 40;
+const int max_zoom = 4;
+const int min_zoom = 1;
+const int start_x_camera = size_window / 2;
+const int start_y_camera = size_window / 2;
+const int size_cell = 30;
 RenderWindow window(VideoMode(size_window, size_window), "shiiit");
 
-void paint_feeld(int size, int x_camera, int y_camera, int zoom) {
+void paint_feeld(int x_camera, int y_camera, int zoom) {
 	VertexArray line_x(Lines, 2);
 	VertexArray line_y(Lines, 2);
-	for (int i = 0; i < size_window / size; i++) {
-		line_x[0].position = sf::Vector2f(i * size * zoom + x_camera, 0);
-		line_x[1].position = sf::Vector2f(i * size * zoom + x_camera, size_window);
-		window.draw(line_x);
-		line_y[0].position = sf::Vector2f(0, i * size * zoom + y_camera);
-		line_y[1].position = sf::Vector2f(size_window, i * size * zoom + y_camera);
-		window.draw(line_y);
+	for (int i = 0; i < size_field_x; i++) {
+		for (int j = 0; j < size_field_y; j++) {
+			RectangleShape rectangle(Vector2f(size_cell * zoom, size_cell * zoom));
+			rectangle.setPosition(i * size_cell * zoom + x_camera, j * size_cell * zoom + y_camera);
+			if ((i + j) % 2 == 0) {
+				rectangle.setFillColor(sf::Color(0, 0, 125));
+			}
+			else {
+				rectangle.setFillColor(sf::Color(125, 0, 0));
+			}
+			
+			window.draw(rectangle);
+			/*line_y[0].position = sf::Vector2f(0, i * size_cell * zoom + y_camera);
+			line_y[1].position = sf::Vector2f(size_window, i * size_cell * zoom + y_camera);
+			window.draw(line_y);*/
+		}
+		
 	}
+}
+
+void change_camera(Event event, Vector2i old_mousePos, Vector2i step, int &x_camera, int &y_camera, int zoom) {
+	Vector2i mousePos;
+	mousePos = Mouse::getPosition(window);
+	step.x = -old_mousePos.x + mousePos.x;
+	step.y = -old_mousePos.y + mousePos.y;
+	x_camera = (x_camera + step.x) % (size_window * zoom);
+	y_camera = (y_camera + step.y) % (size_window * zoom);
+}
+
+void change_zoom(Event event, int &zoom) {
+	if (event.mouseWheel.delta == 1 and zoom < max_zoom) {
+		zoom++;
+	}
+	else if (event.mouseWheel.delta == -1 and zoom > min_zoom)
+		zoom--;
 }
 
 int main()
 {
-	const int size = 30;
+	
 	int x_camera = 0;
 	int y_camera = 0;
 	int zoom = 4;
-	Vector2i mousePos;
 	Vector2i old_mousePos;
 	Vector2i step;
 	while (window.isOpen())
@@ -38,32 +70,17 @@ int main()
 		{
 			
 			if (Mouse::isButtonPressed(Mouse::Middle)){
-				mousePos = Mouse::getPosition(window);
-				step.x = -old_mousePos.x + mousePos.x;
-				step.y = -old_mousePos.y + mousePos.y;
-				if (x_camera < size_window) {
-					x_camera = (x_camera + step.x) % size_window;
-				}
-				if (y_camera < size_window) {
-					y_camera = (y_camera + step.y) % size_window;
-				}
-				cout << x_camera << " " << y_camera << "\n";
+				change_camera(event, old_mousePos, step, x_camera, y_camera, zoom);
 				}
 			if(sf::Event::MouseWheelMoved) {
-				if (event.mouseWheel.delta == 1 and zoom < 4) {
-					zoom++;
-				}
-				else if (event.mouseWheel.delta == -1 and zoom > 1)
-					zoom--;
+				change_zoom(event, zoom);
 			}
 				
 			if (event.type == Event::Closed)
 				window.close();
 			old_mousePos = Mouse::getPosition(window);
 		}
-		
-		paint_feeld(size, x_camera, y_camera, zoom);
-		
+		paint_feeld(x_camera, y_camera, zoom);
 		window.display();
 	}
 
